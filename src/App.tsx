685750beab3c667d1090991120991a2286686c4e
Devesh Kodiswaran Ramiya <dramiya@eceprog4.ecn.purdue.edu>
2023-12-05 20:31:24 -0500

    Finished frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
-import Header from "./components/Header";
-import ButtonGroup from "./components/ButtonGroup";
-import Content from "./components/Content";
-import SubHeader from "./components/SubHeader";
 
-function App(){
+import SearchBar from './components/SearchBar';
+import React, { useState } from 'react';
+import NavigationBar from "./components/NavigationBar";
+import ProjectCard from "./components/ProjectCard";
+import ProjectDescriptionPage from "./components/ProjectDesc";
+import LeftPanel from "./components/LeftPanel";
+function App() {
   
-  let items = ["package1", "package2", "package3"];
-  let package_name = "GitHub Package Name";
-  let package_description = "this is a package";
+  // let items = ["package1", "package2", "package3"];
+  // let package_name = "GitHub Package Name";
+  // let package_description = "this is a package";
 
   const handleSelectItem = (item: string) => {
     console.log(item);
   }
-/*
-    <SearchBar></SearchBar>
-    <Button onClick={()=> console.log('clicked download')}>Download</Button>
-    <Button onClick={()=> console.log('clicked upload')}>Upload</Button>
-    <Button onClick={()=> console.log('clicked update')}>Update</Button>
-    <Button onClick={()=> console.log('clicked rate')}>Rate</Button>
-  } */
-  return <div>
-      <Header />
-      <SubHeader packageName={package_name} />
-      <ButtonGroup />
-      <div className="divider" />
-      <Content packageDescription={package_description} githubLink="github.com"></Content>
+  const [searchTerm, setSearchTerm] = useState('');
 
+  const handleSearch = (searchTerm: string) => {
+    setSearchTerm(searchTerm);
+  };
+  return <div>
+    <NavigationBar ></NavigationBar>
+    <div className="container mx-auto mt-8 py-4">
+      <SearchBar onSearch={handleSearch} />
     </div>
+
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    {/* <ProjectDescriptionPage /> */}
+
+
+  </div >
 }
 
 export default App;
